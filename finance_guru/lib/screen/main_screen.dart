c0177f7583141cab622c0afd50f9ce6814637dce
Bibesh17 <bibeshpokharel17@gmail.com>
2024-08-15 20:43:32 +0545

    shreya

--- finance_guru/lib/screen/main_screen.dart
+++ finance_guru/lib/screen/main_screen.dart
@@ -54,10 +54,127 @@ class MainScreen extends StatelessWidget {
             // container
 
             Container(
-              height: 250,
+              height: 400,
               width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color(0xFF3F7EA4),
+              decoration: BoxDecoration(
+                color: const Color(0xFF32ADE6),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Shrawan 2081',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              color: Colors.white70),
+                        ),
+                        const SizedBox(width: 80),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.search),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.more_vert_outlined),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        height: 50,
+                        width: 110,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.lightBlueAccent),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 39, 159, 196),
+                                  shape: BoxShape.circle),
+                              child:
+                                  const Icon(CupertinoIcons.arrow_down_circle),
+                            ),
+                            const SizedBox(width: 10),
+                            const Column(
+                              children: [
+                                Text(
+                                  'Income',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Rs. 45000",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        height: 50,
+                        width: 110,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.lightBlueAccent),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 20,
+                              width: 20,
+                              decoration: const BoxDecoration(
+                                  color: Color.fromARGB(255, 39, 159, 196),
+                                  shape: BoxShape.circle),
+                              child:
+                                  const Icon(CupertinoIcons.arrow_down_circle),
+                            ),
+                            const SizedBox(width: 10),
+                            const Column(
+                              children: [
+                                Text(
+                                  'Spending',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Rs. 45000",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
